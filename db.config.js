fa6417bcf2da09ff31d07c0ef168959cc47210e1
Elisee-des <esabidani@gmail.com>
2023-05-08 12:59:30 +0000

    Configuration de la base de donner

---
+++ db.config.js
@@ -0,0 +1,19 @@
+/************************************************************/
+/**************** Import des module necessaire **************/
+const { Sequelize } = require('sequelize');
+
+/**************** Connexion a la base de donner *************/
+let sequelize = new Sequelize(
+    process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'mssql',
+        logging: false
+    }
+)
+
+/************************************************************/
+/**************** Synchronisation des modèles *************/
+// sequelize.sync()
+
+module.exports = sequelize;
