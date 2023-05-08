cd2c6186dc5078273b2f25733d2b0d3618d595ee
Elisee-des <esabidani@gmail.com>
2023-05-08 13:31:53 +0000

    Connection a la base de donner

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const express = require('express');
 const cors = require('cors');
 
 /**********************************************************/
+/**** Importation de la connexion a la base de donner ****/
+let DB = require('./db.config');
+
+
+/**********************************************************/
 /******* Initialisation de l'API *******/
 const app = express();
 
@@ -18,7 +23,13 @@ app.get('/', (req, res) => res.send(`J'suis en ligne. Bienvenu ! `));
 app.get('*', (req, res) => res.status(501).send(`Route non determiné`));
 
 /**********************************************************/
-/******* Start serveur *******/
-app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Serveur en lecture sur le port ${process.env.SERVER_PORT}`);
-} )
+/******* Start serveur et test de demarrage *******/
+
+DB.authenticate()
+    .then( () => console.log("Database connection ok"))
+    .then( () => {
+        app.listen(process.env.SERVER_PORT, () => {
+            console.log(`Serveur en lecture sur le port ${process.env.SERVER_PORT}`);
+        })
+    })
+    .catch(err => console.log('Database Error', err))
